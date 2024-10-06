5d69690ab4d0188f6280bad7b9547e4e8e45556d
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-10-07 00:44:48 +0300

    feat(binary_trees): imp Same and tests

--- binary_trees/binary_trees.go
+++ binary_trees/binary_trees.go
@@ -1,6 +1,9 @@
 package main
 
-import "golang.org/x/tour/tree"
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
@@ -14,11 +17,41 @@ func Walk(t *tree.Tree, ch chan int) {
 	}
 }
 
+func testWalk() {
+	var t = tree.New(3)
+	var ch = make(chan int, 10)
+	go Walk(t, ch)
+	fmt.Println(<-ch)
+	close(ch)
+}
+
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	panic("Not implemented")
+	n := 10
+	ch1 := make(chan int, n)
+	ch2 := make(chan int, n)
+	defer close(ch1)
+	defer close(ch2)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := 0; i < n; i++ {
+		val1 := <-ch1
+		val2 := <-ch2
+		if val1 != val2 {
+			return false
+		}
+	}
+	return true
+}
+
+func testSame() {
+	var t1, t2 *tree.Tree = tree.New(1), tree.New(1)
+	var result bool = Same(t1, t2)
+	fmt.Println(result)
 }
 
 func main() {
+	testWalk()
+	testSame()
 }
