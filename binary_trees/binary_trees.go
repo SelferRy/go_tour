0d58bf26dcc77496f680ce46dd4f42e789fe475c
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-10-06 15:49:55 +0300

    feat(binary_trees): imp Walk

--- binary_trees/binary_trees.go
+++ binary_trees/binary_trees.go
@@ -5,7 +5,13 @@ import "golang.org/x/tour/tree"
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
 }
 
 // Same determines whether the trees
